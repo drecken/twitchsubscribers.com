de8ff51702deb2b48f671cd3f58d7866dfd483a1
jakebathman <jake.bathman@gmail.com>
2018-05-04 12:16:47 -0500

    Adjust layout and formatting, and add sub date
    Changes to `subsrcibers.blade.php` include:
    - Adding a header to the table
    - Adding Sub Date column
    - Using Date-fns, render the date in the browser’s local time zone
    - Combine avatar and user fields and center with flex box
    - Allow for “prime” subscriptions to render correctly

--- resources/views/subscribers.blade.php
+++ resources/views/subscribers.blade.php
@@ -1,29 +1,67 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1>You have {{ number_format(count($subscribers), 0) }} subscribers</h1>
-    <table class="highlight">
-        @foreach($subscribers as $subscriber)
-            <tr>
-                <td>
-                    {{ $loop->iteration }}
-                </td>
-                <td>
+<style>
+    .user-info {
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        /* height: 50px; */
+    }
+    .user-info > div{
+        padding: 5px;
+    }
+</style>
+<h1>You have {{ number_format(count($subscribers), 0) }} {{ str_plural('subscriber', count($subscribers)) }}</h1>
+<table class="highlight">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Sub Date</th>
+            <th>User</th>
+            <th>Sub Level</th>
+        </tr>
+    </thead>
+    @foreach($subscribers as $subscriber)
+    <tr>
+        <td>
+            {{ $loop->iteration }}
+        </td>
+        <td>
+            <span class="sub-date">{{ $subscriber['date'] }}</span>
+        </td>
+        <td>
+            <div class="user-info">
+                <div>
                     @if($subscriber['logo'])
-                        <img src="{{ $subscriber['logo'] }}" width="50px">
+                    <img src="{{ $subscriber['logo'] }}" height="50px">
                     @else
-                        <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png" width="50px">
+                    <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png" height="50px">
                     @endif
-                </td>
-                <td>
-                    {{ $subscriber['name'] }} (<a
-                            href="https://www.twitch.tv/{{ $subscriber['displayName'] }}">{{ $subscriber['displayName'] }}</a>
-                    )
-                </td>
-                <td>
-                    Tier {{ $subscriber['tier'] }}
-                </td>
-            </tr>
-        @endforeach
-    </table>
+                </div>
+                <div>
+                    {{ $subscriber['name'] }} (<a href="https://www.twitch.tv/{{ $subscriber['displayName'] }}">{{ $subscriber['displayName'] }}</a>)
+                </div>
+            </div>
+        </td>
+        <td>
+            @if($subscriber['tier'] == 'Prime')
+            Prime                
+            @else
+            Tier {{ $subscriber['tier'] }}
+            @endif
+        </td>
+    </tr>
+    @endforeach
+</table>
+
+<script src="http://cdn.date-fns.org/v1.9.0/date_fns.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $(".sub-date").each(function(v){
+            $(this).html(dateFns.format(new Date($(this).html().trim()), "MMM Do, YYYY @ h:mm a"));
+        });
+        
+    });
+</script>
 @endsection
